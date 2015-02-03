f8852e3a794fa9e486b77fcd21dc24ae7de44b36
Francois Ribalet <ribalet@uw.edu>
2015-02-03 08:29:32 -0800

    checking instrument alignment

--- Rhodo_labExperiment/cell_cycle/CellCycle_Rhodo.R
+++ Rhodo_labExperiment/cell_cycle/CellCycle_Rhodo.R
@@ -1,17 +1,23 @@
 library(flowCore)
 library(flowDensity)
 
-filelist <- list.files(path="Rhodo_labExperiment/cell_cycle/INFLUX_cell_cycle", 
+home <- "/Users/francois/Documents/DATA/SeaFlow/CMOP/CMOP_git/"
+filelist <- list.files(path=paste0(home,"Rhodo_labExperiment/cell_cycle/INFLUX_cell_cycle"), 
                                 pattern='.fcs', recursive=TRUE, full.names=TRUE)
 results <- NULL  
 for(file in filelist) {
         
-        # file <- filelist[27]
+       # file <- filelist[7]
         print(paste("processing file #",basename(file)))
             fcs <- read.FCS(file,transformation= F)
-            data <- Subset(fcs, as.tiffical(exprs(fcs[,4]) > 40000) & as.logical(exprs(fcs[,4]) < 55000))
+            plot(fcs[,c(4,6)])
+            
+            beads <- Subset(fcs, as.logical(exprs(fcs[,4]) < 30000) & as.logical(exprs(fcs[,6]) > 35000))
+            med.beads <- summary(beads[,2])[3]
 
-                res <- flowDensity(data, channels=c(2,7), position=c(FALSE,FALSE), upper=c(T,NA),sd.threshold=c(T,T),  n.sd=c(1,1))
+            data <- Subset(fcs, as.logical(exprs(fcs[,4]) > 40000) & as.logical(exprs(fcs[,4]) < 55000))
+
+                res <- flowDensity(data/med.beads, channels=c(2,7), position=c(FALSE,FALSE), upper=c(T,NA),sd.threshold=c(T,T),  n.sd=c(1,1))
                     
                     png(paste(basename(file),".png",sep=""),width=89, height=89, units='mm',res=600, pointsize=5) 
                         plot(data , res)
